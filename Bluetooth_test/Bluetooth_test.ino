19cb4bacf485d5c4f46141b912b1076233c4fcb0
YCH20187613 <127296398+YCH20187613@users.noreply.github.com>
2023-04-20 17:06:20 +0800

    Update Bluetooth_test.ino

--- Bluetooth_test/Bluetooth_test.ino
+++ Bluetooth_test/Bluetooth_test.ino
@@ -1,25 +1,38 @@
 #include <SoftwareSerial.h>
+#include <Servo.h>
 
-const uint8_t module_outputpin = 3;
-const uint8_t module_inputpin = 2;
+const uint8_t module_outputpin = 3; //setting output pin
+const uint8_t module_inputpin = 2; // setting input pin
 
-SoftwareSerial bluth(module_inputpin, module_outputpin);
+Servo myservo; 
+
+SoftwareSerial bluth(module_inputpin, module_outputpin); //create software serial
 
 void setup() {
   // put your setup code here, to run once:
-  bluth.begin(9600);
-  Serial.begin(9600);
-  bluth.print("AT+NAME=BMbluth");
-
+  bluth.begin(9600); //begin the bluetooth module on 9600 baud rate
+  Serial.begin(9600); //begin the serial module on 9600 baud rate
+  bluth.print("AT+NAME=BMbluth"); //AT commands to configurate the board
+  bluth.print("AT+UUID0xFFE0");
+  bluth.print("AT+CHAR0xFFE1");
+  myservo.attach(9);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (bluth.available()) {
-    Serial.write(bluth.read());
+    Serial.write(bluth.read()); //start reading data
+    
   }
 
   if (Serial.available()) {
     bluth.write(Serial.read());
+    
   }
+    
+    
+    
+    
+  
 }
