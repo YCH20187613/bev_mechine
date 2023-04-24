1feccd0406db744362c3bd7fc0d038e116e827ff
YCH20187613 <127296398+YCH20187613@users.noreply.github.com>
2023-04-24 17:08:39 +0800

    Update Bluetooth_test.ino

--- Bluetooth_test/Bluetooth_test.ino
+++ Bluetooth_test/Bluetooth_test.ino
@@ -1,10 +1,12 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
+#include <Unistep2.h>
 
 const uint8_t module_outputpin = 3; //setting output pin
 const uint8_t module_inputpin = 2; // setting input pin
-
+const int step = 2048;
 Servo myservo; 
+Unistep2 mystepper(8, 9, 10, 11, 4096, 1000);
 
 SoftwareSerial bluth(module_inputpin, module_outputpin); //create software serial
 
@@ -15,14 +17,16 @@ void setup() {
   bluth.print("AT+NAME=BMbluth"); //AT commands to configurate the board
   bluth.print("AT+UUID0xFFE0");
   bluth.print("AT+CHAR0xFFE1");
-  myservo.attach(9);
+  
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (bluth.available()) {
+    String readBuffer = "";
     Serial.write(bluth.read()); //start reading data
+    
     
   }
 
@@ -30,7 +34,9 @@ void loop() {
     bluth.write(Serial.read());
     
   }
-    
+  
+  
+  
     
     
     
